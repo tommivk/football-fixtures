a2386bc85403685c64cfd99126a41481384fb86f
tommivk <tommikokk@gmail.com>
2023-01-25 12:10:00 +0200

    add loading progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from "next/app";
 import Layout from "../components/Layout";
+import NextNProgress from "nextjs-progressbar";
 
 import "../static/styles.css";
 
@@ -7,6 +8,14 @@ export default function MyApp({ Component, pageProps }: AppProps<{}>) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <NextNProgress
+        color="#29D"
+        startPosition={0.3}
+        stopDelayMs={300}
+        height={1}
+        showOnShallow={true}
+        options={{ showSpinner: false, easing: "ease" }}
+      />
     </Layout>
   );
 }
