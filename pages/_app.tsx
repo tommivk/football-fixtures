0b55fc690c61a2917d9db8b6863f23b81c2af4e5
tommivk <tommikokk@gmail.com>
2023-01-25 06:44:09 +0200

    add _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import { AppProps } from "next/app";
+import Layout from "../components/Layout";
+
+import "../static/styles.css";
+
+export default function MyApp({ Component, pageProps }: AppProps<{}>) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
