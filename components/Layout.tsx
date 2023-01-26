b5a210f7e6129b7fbf98613788db60395603e89b
tommivk <tommikokk@gmail.com>
2023-01-26 09:19:51 +0200

    prevent scrollbar from moving content

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,9 +6,9 @@ type Props = {
 
 export default function Layout({ children }: Props) {
   return (
-    <>
+    <div className="layout">
       <LeagueCards />
       <main>{children}</main>
-    </>
+    </div>
   );
 }
