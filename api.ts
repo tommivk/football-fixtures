7947c37436a43b7c5398f60f56bbae9b559e26b2
tommivk <tommikokk@gmail.com>
2023-06-28 15:25:01 +0300

    remove unnecessary try/catch

--- api.ts
+++ api.ts
@@ -9,81 +9,68 @@ const BASE_URL = "https://v3.football.api-sports.io";
 const currentSeason = 2022;
 
 export const fetchData = async (path: string, cacheTimeMinutes?: number) => {
-  try {
-    const cache = await redis.get(path);
-    // await redis.flushall();
-    // await redis.del(path);
-    if (cache) {
-      console.log("cached ", path);
-      return JSON.parse(cache);
-    }
-
-    console.log("no cache ", path);
-
-    const requestHeaders: HeadersInit = new Headers();
-    requestHeaders.set("x-rapidapi-key", API_KEY);
-
-    const response = await fetch(`${BASE_URL}${path}`, {
-      headers: requestHeaders,
-    });
-
-    if (!response.ok) {
-      throw new Error("Failed to fetch");
-    }
-
-    const json = await response.json();
-    if (!(Array.isArray(json.errors) && json.errors.length === 0)) {
-      throw json.errors;
-    }
-
-    const data = camelcaseKeys(json.response, { deep: true });
-
-    cacheTimeMinutes !== undefined
-      ? await redis.set(path, JSON.stringify(data), "EX", 60 * cacheTimeMinutes)
-      : await redis.set(path, JSON.stringify(data));
-
-    return data;
-  } catch (error) {
-    console.error(error);
-    throw error;
+  const cache = await redis.get(path);
+  // await redis.flushall();
+  // await redis.del(path);
+  if (cache) {
+    console.log("cached ", path);
+    return JSON.parse(cache);
   }
+
+  console.log("no cache ", path);
+
+  const requestHeaders: HeadersInit = new Headers();
+  requestHeaders.set("x-rapidapi-key", API_KEY);
+
+  const response = await fetch(`${BASE_URL}${path}`, {
+    headers: requestHeaders,
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch");
+  }
+
+  const json = await response.json();
+  if (!(Array.isArray(json.errors) && json.errors.length === 0)) {
+    throw json.errors;
+  }
+
+  const data = camelcaseKeys(json.response, { deep: true });
+
+  cacheTimeMinutes !== undefined
+    ? await redis.set(path, JSON.stringify(data), "EX", 60 * cacheTimeMinutes)
+    : await redis.set(path, JSON.stringify(data));
+
+  return data;
 };
 
 export const getAllTeams = async () => {
-  try {
-    const result = await Promise.all(
-      supportedLeagueIds.map((id) =>
-        fetchData(`/teams?league=${id}&season=${currentSeason}`, 43800)
-      )
-    );
-    let allTeams = result.flatMap((r) => r);
-    //remove duplicates
-    allTeams = allTeams.filter(
-      (value, index, self) =>
-        index === self.findIndex((t) => t.team.id === value.team.id)
-    );
-    return allTeams;
-  } catch (error) {
-    throw error;
-  }
+  const result = await Promise.all(
+    supportedLeagueIds.map((id) =>
+      fetchData(`/teams?league=${id}&season=${currentSeason}`, 43800)
+    )
+  );
+  let allTeams = result.flatMap((r) => r);
+  //remove duplicates
+  allTeams = allTeams.filter(
+    (value, index, self) =>
+      index === self.findIndex((t) => t.team.id === value.team.id)
+  );
+  return allTeams;
 };
 
 export const getStandingsByLeagueId = async (leagueId: string) => {
-  try {
-    if (leagueId === "2") return []; // Ignore Champions League
+  if (leagueId === "2") return []; // Ignore Champions League
 
-    const result: StandingsResult[] = await fetchData(
-      `/standings?league=${leagueId}&season=${currentSeason}`,
-      24 * 60
-    );
-    const standings = result?.[0].league?.standings;
-    if (!standings || standings.length === 0) {
-      throw "Standings were undefined";
-    }
-    return standings[0];
-  } catch (error) {
-    throw error;
+  const result: StandingsResult[] = await fetchData(
+    `/standings?league=${leagueId}&season=${currentSeason}`,
+    24 * 60
+  );
+  const standings = result?.[0].league?.standings;
+  if (!standings || standings.length === 0) {
+    throw "Standings were undefined";
   }
+  return standings[0];
 };
 
 export const getFixturesByLeagueId = async (leagueId: string) => {
